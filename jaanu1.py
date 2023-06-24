bffa3fb2b2a970902567d900a773004f5e1342c8
jeohmsptcs <137598837+jeohmsptcs@users.noreply.github.com>
2023-06-24 12:38:07 +0530

    Add files via upload

---
+++ jaanu1.py
@@ -0,0 +1,18 @@
+Python 3.11.3 (tags/v3.11.3:f3909b8, Apr  4 2023, 23:49:59) [MSC v.1934 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+===================== RESTART: C:/Users/LENEVO 06/jaanu.py =====================
+the sum 2 inteajers is: 1000
+>>> 
+>>> 
+>>> str1="HMS PLYTECHNIC
+SyntaxError: incomplete input
+>>> 
+=================== RESTART: C:/Users/LENEVO 06/jaanumanu.py ===================
+the sum of 2 inteajers is: 875
+>>> 
+>>> str1="HMS POLYTECNIC"
+>>> str2="TUMKUR"
+>>> sum=str1+" "+str2
+>>> print("the sum of 2 strings is:",sum)
+the sum of 2 strings is: HMS POLYTECNIC TUMKUR
